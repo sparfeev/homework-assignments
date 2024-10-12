3379ef701228b8a08bf632179fdebb2dcf356193
sparfeev <sparfeev95@gmail.com>
2024-10-12 13:15:33 +0800

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,5 @@
+homework_assignment = 12
+hours_spent = 1.5
+Course_name = 'Курс: Paython,'
+time_for_the_task = 1.5/12
+print(Course_name, 'всего задач:', homework_assignment , ',затрачено часов:', hours_spent, ',среднее время выполнения:', time_for_the_task ,'часов.')
