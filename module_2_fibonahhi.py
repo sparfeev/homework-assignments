963a1d74c5cf63c6da664b2f13aa853918e5ec9b
sparfeev <sparfeev95@gmail.com>
2024-11-01 21:18:35 +0800

    Add files via upload

---
+++ module_2_fibonahhi.py
@@ -0,0 +1,31 @@
+fib1 = 1
+fib2 = 1
+n = int(input('Номер элемента ряда Фибоначчи: '))
+
+i = 0
+while i < n - 2:
+    fib_sum = fib1 + fib2
+    fib1 = fib2
+    fib2 = fib_sum
+    i = i + 1
+print('Значение этого элемента:', fib2)
+
+
+def fibonacci_recursive(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+
+
+def fibonacci_iterative(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    a, b = 0, 1
+    for _ in range(2, n+1):
+        a, b = b, a + b
+    return b
