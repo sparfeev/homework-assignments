33ce1b406999216d6fe20b530e30701d8f2a42dc
sparfeev <sparfeev95@gmail.com>
2024-11-06 19:48:36 +0800

    Add files via upload

--- module_2_5.py
+++ module_2_5.py
@@ -1,9 +1,12 @@
 def get_matrix(n,m,value):
     matrix = [[value] * m for i in range(n)]
-    print(matrix)
+    return(matrix)
 
 
-get_matrix(3,4,66)
-get_matrix(2,2,10)
-get_matrix(5,4,72)
-get_matrix(0,0,0)
+
+result1 = get_matrix(3,4,66)
+print(result1)
+result2 = get_matrix(2,2,10)
+print(result2)
+result3 = get_matrix(5,4,72)
+print(result3)
