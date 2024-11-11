4bc85984640e53cc51805855b2a0ec7b10d62443
sparfeev <sparfeev95@gmail.com>
2024-11-11 21:38:34 +0800

    Add files via upload

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,10 +1,13 @@
-list1_ = [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-list2_ =[]
-for i in list1_:
-    for j in range(1,20):
-        for n in range(2,20):
-            if n <= j:
-                continue
-            if i % (j + n) == 0:
-                print(f'{i} = {j}{n}')
+n = int(input('Введите целое число от 3 до 20: '))
+password = ''
+for i in range(1, n):
+    for j in range(2, n):
+        if j <= i:
+            continue
+        if n % (i + j) == 0:
+            password += str(i) + str(j)
+
+print('Пароль:',password )
+
+
 
